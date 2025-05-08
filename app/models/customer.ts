f510be82f528d6181ada48e2b35d28459ed300e2
Eric Vondee <ericvondee5@gmail.com>
2025-05-08 17:13:47 +0100

    feat: enhance customer creation and model relationships
    - Updated createCustomer method to associate new customers with the authenticated user.
    - Added userId column to Customer model and established relationships with User, Job, and Invoice models.
    - Removed unnecessary console log from dashboard method.

--- app/models/customer.ts
+++ app/models/customer.ts
@@ -1,8 +1,14 @@
-import { BaseModel, column } from '@adonisjs/lucid/orm'
+import { BaseModel, belongsTo, column, hasMany } from '@adonisjs/lucid/orm'
+import type { BelongsTo, HasMany } from '@adonisjs/lucid/types/relations'
 import type { DateTime } from 'luxon'
+import Invoice from './invoice.js'
+import Job from './job.js'
+import User from './user.js'
 
 export default class Customer extends BaseModel {
   @column({ isPrimary: true }) declare id: string
+
+  @column() declare userId: string
 
   @column() declare fullName: string
 
@@ -23,6 +29,12 @@ export default class Customer extends BaseModel {
   @column() declare businessName: string
 
   @column() declare businessAddress: string
+
+  @belongsTo(() => User) declare user: BelongsTo<typeof User>
+
+  @hasMany(() => Job) declare jobs: HasMany<typeof Job>
+
+  @hasMany(() => Invoice) declare invoices: HasMany<typeof Invoice>
 
   @column.dateTime({ autoCreate: true })
   declare createdAt: DateTime
