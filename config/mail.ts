9781bdd347ded2f61762dc9ecf5c8d2c2a565d30
Eric Vondee <ericvondee5@gmail.com>
2025-05-15 04:31:02 +0100

    feat: update mail configuration to use SMTP with environment variables

--- config/mail.ts
+++ config/mail.ts
@@ -2,19 +2,29 @@ import env from '#start/env'
 import { defineConfig, transports } from '@adonisjs/mail'
 
 const mailConfig = defineConfig({
-  default: 'resend',
+  default: 'smtp',
+  from: env.get('MAIL_FROM') || 'no-reply@togetha.com',
 
-   /**
-    * The mailers object can be used to configure multiple mailers
-    * each using a different transport or same transport with different
-    * options.
+  /**
+   * The mailers object can be used to configure multiple mailers
+   * each using a different transport or same transport with different
+   * options.
    */
-  mailers: {      
+  mailers: {
+    smtp: transports.smtp({
+      host: env.get('SMTP_HOST'),
+      port: env.get('SMTP_PORT'),
+      auth: {
+        type: 'login',
+        user: env.get('SMTP_USERNAME'),
+        pass: env.get('SMTP_PASSWORD'),
+      },
+    }),
+
     resend: transports.resend({
       key: env.get('RESEND_API_KEY'),
       baseUrl: 'https://api.resend.com',
     }),
-    
   },
 })
 
